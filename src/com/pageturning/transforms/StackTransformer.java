47cd525f8bbd7855b7be874f6cda8135f070fc62
sai <sai.wu@bigkoo.com>
2014-12-23 14:11:32 +0800

    Simple and convenient ViewPager 1.0
    实现一行代码配置Viewpager，可定义动画

---
+++ src/com/pageturning/transforms/StackTransformer.java
@@ -0,0 +1,12 @@
+package com.pageturning.transforms;
+
+import android.view.View;
+
+public class StackTransformer extends ABaseTransformer {
+
+	@Override
+	protected void onTransform(View view, float position) {
+		view.setTranslationX(position < 0 ? 0f : -view.getWidth() * position);
+	}
+
+}
