065a10907fa9496f60b359248992eb996fffd6c2
djy <137359049@qq.com>
2018-02-01 19:05:41 +0800

    1.reuse page adapter view，solve always create a child view issue.

--- convenientbanner/src/main/java/com/bigkoo/convenientbanner/adapter/CBPageAdapter.java
+++ convenientbanner/src/main/java/com/bigkoo/convenientbanner/adapter/CBPageAdapter.java
@@ -9,22 +9,86 @@ import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.bigkoo.convenientbanner.holder.Holder;
 import com.bigkoo.convenientbanner.view.CBLoopViewPager;
 
+import java.util.LinkedList;
 import java.util.List;
 
-@SuppressWarnings("WeakerAccess")
+@SuppressWarnings({"WeakerAccess", "unchecked"})
 public class CBPageAdapter<T> extends PagerAdapter {
     protected List<T> mDatas;
     protected CBViewHolderCreator holderCreator;
     private boolean canLoop = true;
     private CBLoopViewPager viewPager;
     private static final int MULTIPLE_COUNT = 300;
+    private LinkedList<View> mViewCache;   // view cache
+
+    private int mChildCount;
+    private int mInvalidatedCount;
+
+    public CBPageAdapter(CBViewHolderCreator holderCreator, List<T> datas) {
+        this.holderCreator = holderCreator;
+        this.mDatas = datas;
+        this.mViewCache = new LinkedList<>();
+
+        if (datas == null) {
+            this.mChildCount = 0;
+        } else {
+            this.mChildCount = datas.size();
+        }
+        this.mInvalidatedCount = 0;
+    }
 
     public int toRealPosition(int position) {
-        int realCount = getRealCount();
+        final int realCount = getRealCount();
         if (realCount == 0) {
             return 0;
         }
         return position % realCount;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        if (mInvalidatedCount > 0) {
+            mInvalidatedCount--;
+            return POSITION_NONE;
+        } else {
+            return super.getItemPosition(object);
+        }
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        final int oldCount = mChildCount;
+        final int newCount = getRealCount();
+        if (oldCount == 0) {
+            // Both children count 0, no change.
+            if (newCount == 0) {
+                return;
+            }
+            // Reset adapter. notifyDataSetChanged() is not working here
+            else {
+                viewPager.setAdapter(this);
+            }
+        } else {
+            // call super notifyDataSetChanged()
+            if (newCount == 0) {
+                mInvalidatedCount = oldCount;
+                super.notifyDataSetChanged();
+            }
+            // Both children are greater than 0. notifyDataSetChanged() is still not working at some time. We donnot know why.
+            // So we call setAdapter() again.
+            else {
+                final int oldPos = viewPager.getCurrentItem();
+                int newPos = oldPos % mChildCount;
+                if (newPos == 0) {
+                    newPos = getRealCount();
+                } else if (newPos >= newCount) {
+                    newPos = newCount - 1;
+                }
+                viewPager.setAdapter(this);
+                viewPager.setCurrentItem(newPos, false);
+            }
+        }
+        mChildCount = newCount;
     }
 
     @Override
@@ -39,7 +103,7 @@ public class CBPageAdapter<T> extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         final int realPosition = toRealPosition(position);
-        final View view = getView(realPosition, null, container);
+        final View view = getView(realPosition, container);
         container.addView(view);
         return view;
     }
@@ -48,6 +112,7 @@ public class CBPageAdapter<T> extends PagerAdapter {
     public void destroyItem(ViewGroup container, int position, Object object) {
         final View view = (View) object;
         container.removeView(view);
+        this.mViewCache.add(view);
     }
 
     @Override
@@ -60,7 +125,8 @@ public class CBPageAdapter<T> extends PagerAdapter {
         }
         try {
             viewPager.setCurrentItem(position, false);
-        } catch (IllegalStateException ignored) {
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
         }
     }
 
@@ -77,19 +143,15 @@ public class CBPageAdapter<T> extends PagerAdapter {
         this.viewPager = viewPager;
     }
 
-    public CBPageAdapter(CBViewHolderCreator holderCreator, List<T> datas) {
-        this.holderCreator = holderCreator;
-        this.mDatas = datas;
-    }
-
-    @SuppressWarnings("unchecked")
-    public View getView(int position, View view, ViewGroup container) {
+    public View getView(int position, ViewGroup container) {
         Holder holder;
-        if (view == null) {
+        View view;
+        if (mViewCache.isEmpty()) {
             holder = (Holder) holderCreator.createHolder();
             view = holder.createView(container.getContext());
             view.setTag(R.id.cb_item_tag, holder);
         } else {
+            view = mViewCache.removeFirst();
             holder = (Holder<T>) view.getTag(R.id.cb_item_tag);
         }
         if (mDatas != null && !mDatas.isEmpty()) {
